66dece15c3ccf8d703958d25fb57394cfa84ef87
Tony Spiro <tspiro@tonyspiro.com>
2020-07-16 16:11:37 -0500

    EDITED: GA env var

--- gatsby-config.js
+++ gatsby-config.js
@@ -57,7 +57,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: process.env.GOOGLE_ANALYTICS_CODE,
+        trackingId: process.env.GOOGLE_ANALYTICS_ID,
       },
     },
     {
