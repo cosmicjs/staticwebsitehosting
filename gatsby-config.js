e40e5cec5b0a2913146a8caedcb72ff2a5b2d687
Tony Spiro <tspiro@tonyspiro.com>
2020-07-16 16:10:29 -0500

    ADDED: GA code

--- gatsby-config.js
+++ gatsby-config.js
@@ -57,7 +57,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-47527341-6`,
       },
     },
     {
